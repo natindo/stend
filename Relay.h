ac54807e154e69804d65d892deb7aea41e941c4d
natindo <lyskov256@gmail.com>
2023-08-25 16:09:47 +0300

    save position relay

--- Relay.h
+++ Relay.h
@@ -3,6 +3,7 @@
 
 #include <Arduino.h>
 #include <string>
+#include "esp_structs.h"
 #define _GLIBCXX_USE_CXX11_ABI 0
 
 class Relay {
@@ -10,8 +11,8 @@ public:
     Relay(uint8_t r1, uint8_t r2, uint8_t r3, uint8_t r4);
     void relayHandler(int relayNum, char relayCommand);
     std::string getStatus (int relayNum);
-    void turnON(int relayNum);
-    void turnOFF(int relayNum);
+    void turnON(int relayNum, RelayData consumers[RELAYS_NUMBER]);
+    void turnOFF(int relayNum, RelayData consumers[RELAYS_NUMBER]);
     std::string switchState(int relayNum);
 
 private:
